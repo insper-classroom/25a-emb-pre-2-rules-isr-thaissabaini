bba6d6da3cd8f501f436b951fc6e87e578480707
thaissabaini <thaissc3@al.insper.edu.br>
2025-02-16 20:11:03 -0300

    update

--- main_isr_kiss.c
+++ main_isr_kiss.c
@@ -2,30 +2,29 @@
 
 volatile int g_cnt = 0;
 volatile char g_str[10];
+volatile int btn_flag = 0;
 
-// This code creates a progress bar on an OLED screen that
-// increases when the button is pressed.
+// Callback de interrupção do botão
 void btn_callback(void) {
-  printf("btn pressed \n");
-
-  if (g_cnt >= 8)
-    g_cnt = 0;
-
-  g_cnt = 2;
-  int i = 0;
-  for (i = 0; i < g_cnt; i++) {
-    g_str = 1;
-    g_str[i] = '*';
-    g_str[i + 1] = NULLL;
-    delay_ms(50);
-    gfx_mono_draw_string(g_str, 0, 0, &sysfont);
-  }
+  btn_flag = 1;
 }
 
 void main(void) {
-  // ...
-  int batata;
-  batata = batata + 1;
   while (1) {
+    if (btn_flag) {
+      btn_flag = 0;
+      printf("btn pressed \n");
+
+      if (g_cnt >= 8)
+        g_cnt = 0;
+      
+      g_cnt = 2;
+      for (int i = 0; i < g_cnt; i++) {
+        g_str[i] = '*';
+      }
+      g_str[g_cnt] = '\0';
+      
+      gfx_mono_draw_string(g_str, 0, 0, &sysfont);
+    }
   }
 }
